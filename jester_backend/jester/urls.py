2c97cc992eca84ab5fc81ec1c8a0bd4e2f9fc010
Viraj Mahesh <virajmahesh@gmail.com>
2015-01-19 17:27:10 -0800

    Added regex matcher for registration
    Former-commit-id: 562dfb467222c11c897f3e6ded54b257bacae0fb

--- jester_backend/jester/urls.py
+++ jester_backend/jester/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
     url(r'^new_user/$', views.new_user),
     url(r'^request_joke/(?P<user_id>\d+)/$', views.request_joke),
     url(r'^rate_joke/(?P<user_id>\d+)/(?P<joke_id>\d+)/(?P<rating>\d+)/$', views.rate_joke),
-    url(r'^register_user/(?P<email>\s)/(?P<password>\s)', views.register_user),
+    url(r'^register_user/(?P<email>\S+)/(?P<password>\S+)', views.register_user),
 )
