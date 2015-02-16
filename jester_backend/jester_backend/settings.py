69ac76cfb2e05651b25e7a6e773520e5c8e8e845
Viraj Mahesh <virajmahesh@gmail.com>
2015-02-16 11:45:45 -0800

    Added CORS middleware

--- jester_backend/jester_backend/settings.py
+++ jester_backend/jester_backend/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jester',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
