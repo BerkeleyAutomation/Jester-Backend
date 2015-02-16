cfbc08afa0ec8ad00ccb125075a14c107ac4891c
Viraj Mahesh <virajmahesh@gmail.com>
2015-02-16 03:34:41 -0800

    Added script to clear database

---
+++ jester_backend/scripts/clear_database.sql
@@ -0,0 +1,11 @@
+/* Delete all rows from tables */
+DELETE FROM jester_user;
+DELETE FROM jester_joke;
+DELETE FROM jester_rating;
+DELETE FROM jester_recommendermodel;
+
+/* Reset auto increment fields */
+ALTER TABLE jester_user AUTO_INCREMENT = 1;
+ALTER TABLE jester_joke AUTO_INCREMENT = 1;
+ALTER TABLE jester_rating AUTO_INCREMENT = 1;
+ALTER TABLE jester_recommendermodel AUTO_INCREMENT = 1;
