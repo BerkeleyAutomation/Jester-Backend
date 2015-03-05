c53ce3fd479edf00ca92d96ed5b31676a93901d0
Viraj Mahesh <virajmahesh@gmail.com>
2015-03-05 12:00:36 -0800

    Updated table names in clear_database.sql

--- jester_backend/scripts/clear_database.sql
+++ jester_backend/scripts/clear_database.sql
@@ -1,6 +1,6 @@
 /* Delete all rows from tables */
-DELETE FROM jester_useraction;
-DELETE FROM jester_recommenderaction;
+DELETE FROM jester_userlog;
+DELETE FROM jester_recommenderlog;
 DELETE FROM jester_rating;
 DELETE FROM jester_rater;
 DELETE FROM jester_joke;
@@ -13,5 +13,5 @@ ALTER TABLE jester_joke AUTO_INCREMENT = 1;
 ALTER TABLE jester_rating AUTO_INCREMENT = 1;
 ALTER TABLE jester_recommendermodel AUTO_INCREMENT = 1;
 ALTER TABLE auth_user AUTO_INCREMENT = 1;
-ALTER TABLE jester_useraction AUTO_INCREMENT = 1;
-ALTER TABLE jester_recommenderaction AUTO_INCREMENT = 1;
+ALTER TABLE jester_userlog AUTO_INCREMENT = 1;
+ALTER TABLE jester_recommenderlog AUTO_INCREMENT = 1;
