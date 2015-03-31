65f670ce2e9e55fe151366c76e68a10ef7a02ab3
root <root@rieff.IEOR.Berkeley.EDU>
2015-03-30 22:46:27 -0700

    add gauge boolean to request_joke

--- jester_backend/jester/views.py
+++ jester_backend/jester/views.py
@@ -74,7 +74,7 @@ def request_joke(request):
     log_request_joke(request, user, joke, stale, random, gauge)
 
     # Return the joke as a JSON object
-    response = {'joke_id': joke.id, 'joke_text': joke.joke_text}
+    response = {'joke_id': joke.id, 'joke_text': joke.joke_text, 'gauge': int(gauge)}
     return HttpResponse(json.dumps(response), content_type="application/json")
 
 
